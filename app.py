9bb1a6c9cac468e7c548a3dd1330a446a95ff675
Gokul Jayaprakash <260779878+gokuljayaprakash8@users.noreply.github.com>
2026-07-27 11:55:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -122,10 +122,10 @@ def audit(req: WorkflowRequest):
     results = []
 
     for step in req.steps:
-      try:
-         decision = svp_kernel(step)
-       except Exception as e:
-         return {"error": str(e)}
+       try:
+          decision = svp_kernel(step)
+        except Exception as e:
+          return {"error": str(e)}
 
         audit_event = audit_logger.create_event(decision, "1.0.0")
         audit_logger.save_event(audit_event)
